175279dd49e2ce5926706964356cbdf49e65bf84
Yoonju Jeon <40294277+yoonju3221@users.noreply.github.com>
2024-05-29 15:14:38 +0900

    Update Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,35 +1,9 @@
 import { useEffect, useState } from 'react';
-import { getPostById, getPostList } from '../api';
+import { getPostList } from '../api';
 import PostListItem from '../components/PostListItem';
-import { IResponsePostList, TAG } from '../api/types';
+import { IResponsePostList} from '../api/types';
 import NoPostList from '../components/NoPostList';
 
-const list = [
-  {
-    post: {
-      id: 1,
-      title: '1번 게시글',
-      contents: '내용',
-      tag: TAG.REACT,
-    },
-  },
-  {
-    post: {
-      id: 2,
-      title: '2번 게시글',
-      contents: '내용',
-      tag: TAG.REACT,
-    },
-  },
-  {
-    post: {
-      id: 3,
-      title: '3번 게시글',
-      contents: '내용',
-      tag: TAG.REACT,
-    },
-  },
-];
 
 const Home = () => {
   const [postList, setPostList] = useState<IResponsePostList>([]);
